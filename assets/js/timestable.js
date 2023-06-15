d7b8995c3803c56a65b58c7186458b122b47b7cb
hemal <20095369@tafe.wa.edu.au>
2023-06-15 16:01:03 +0800

    time table

--- assets/js/timestable.js
+++ assets/js/timestable.js
@@ -16,5 +16,41 @@ document.getElementById('timesTableSelect').addEventListener('change', function(
 //   var option = document.createElement('option');
 //   option.value = i;
 //   option.text = i;
-//   timesTableSelectContainer.appendChild(option);
+// //   timesTableSelectContainer.appendChild(option);
+// // }
+
+
+
+// var timesTableSelectContainer = document.getElementById('timesTableSelectContainer');
+//
+// // Generate the times tables options dynamically
+// var table = document.createElement('table');
+// for (var i = 1; i <= 9; i++) {
+//     var row = document.createElement('tr');
+//
+//     var cell = document.createElement('td');
+//     var option = document.createElement('option');
+//     option.value = i;
+//     option.text = i;
+//     cell.appendChild(option);
+//     row.appendChild(cell);
+//
+//     table.appendChild(row);
 // }
+//
+// timesTableSelectContainer.appendChild(table);
+//
+// document.getElementById('timesTableSelectContainer').addEventListener('change', function() {
+//     var selectedValue = parseInt(this.value);
+//     var timesTableOutput = document.getElementById('timesTableOutput').getElementsByTagName('tbody')[0];
+//
+//     timesTableOutput.innerHTML = '';
+//
+//     for (var i = 1; i <= 10; i++) {
+//         var result = selectedValue * i;
+//         var row = document.createElement('tr');
+//         row.innerHTML = '<td>' + selectedValue + '</td><td>' + i + '</td><td>' + result + '</td>';
+//         timesTableOutput.appendChild(row);
+//     }
+// });
+//
